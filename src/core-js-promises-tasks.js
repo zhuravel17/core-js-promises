71da3069df9d24d6d345e4baad38bc139cd8df64
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-04-14 23:14:39 +0400

    feat: add getAllOrNothing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -107,8 +107,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
