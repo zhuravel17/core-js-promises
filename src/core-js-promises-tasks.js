09909ce3e9c4b8da23f291111985c015db2a03ad
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-04-14 23:17:26 +0400

    feat: add queuePromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -149,8 +149,20 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let result = '';
+  function processNext(index) {
+    if (index >= promises.length) {
+      return Promise.resolve(result);
+    }
+
+    return promises[index].then((value) => {
+      result += String(value);
+      return processNext(index + 1);
+    });
+  }
+
+  return processNext(0);
 }
 
 module.exports = {
