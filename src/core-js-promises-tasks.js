a44bedf84cfa7245665d7675fd4b6708f0d0cec3
zhuravel17 <zhuravlevakat1996@gmail.com>
2025-04-14 22:50:20 +0400

    feat: add getPromise

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,14 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 0) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
 
 /**
